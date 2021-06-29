49380d207bd3e8d0e4c539ebfead7c83c4893710
Sara-Issa <85680247+Sara-Issa@users.noreply.github.com>
2021-06-29 19:14:07 +0300

    done

--- Part-1/challenge.js
+++ Part-1/challenge.js
@@ -1,2 +1,9 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+var payment = 195;
+
+if (50 >= payment && payment >= 200) {
+  console.log(payment * 0.15);
+} else if (200 > payment) {
+  console.log(payment * 0.2);
+} else {
+  console.log("no tip");
+}
