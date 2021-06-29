711cf29b7bccaf959eed4586515865455a6f5069
i7usen <85677008+i7usen@users.noreply.github.com>
2021-06-29 16:00:23 +0300

    done

--- Part-1/challenge.js
+++ Part-1/challenge.js
@@ -1,2 +1,18 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+var bill = prompt("Enter bill amount ?");
+
+var tip;
+
+if (bill >= 50 && bill <= 300) {
+  tip = bill * 0.15;
+  console.log("tip for 15%", tip);
+} else {
+  tip = bill * 0.2;
+  console.log("tip for 20%", tip);
+}
+
+var total = bill + tip;
+
+console.log(`the bill was ${bill} , the tip ${tip}, and the value is ${total}`);
